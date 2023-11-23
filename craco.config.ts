ddda238f43f547334f4177d07f21a2ca1b2fb96e
Lilian Marzet <lilian.marzet123@gmail.com>
2023-11-23 14:20:31 +0100

    Initial commit

---
+++ craco.config.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+import CracoAlias from "craco-alias";
+
+module.exports = {
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: "tsconfig",
+                baseUrl: "./src",
+                tsConfigPath: "./tsconfig.paths.json",
+            },
+        },
+    ],
+};
---
+++ src/ui/views/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+      <div>
+          <p>
+              HomeScreen
+          </p>
+      </div>
+  )
+}
+
+export default HomeScreen
