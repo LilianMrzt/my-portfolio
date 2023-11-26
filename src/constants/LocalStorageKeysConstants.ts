f2089164c8c394422ac19a9629b1a43abb474a59
Lilian Marzet <lilian.marzet123@gmail.com>
2023-11-26 03:04:14 +0100

    [EVOL] Création de constantes pour la maintenabilité du code

---
+++ src/constants/LocalStorageKeysConstants.ts
@@ -0,0 +1,3 @@
+export const LocalStorageKeysConstants = {
+  theme: 'themeKey'
+}
--- src/utils/UseThemeHook.ts
+++ src/utils/UseThemeHook.ts
@@ -1,20 +1,20 @@
 import { useEffect, useState } from 'react'
 import { type ChangeEvent } from 'react'
 import { type ThemeType } from '@interfaces/types/ThemeType'
+import { LocalStorageKeysConstants } from '@constants/LocalStorageKeysConstants'
+import { Theme } from '@constants/Constants'
 
 type useThemeReturn = [ThemeType, (e: ChangeEvent<HTMLInputElement>) => void]
 
-const localStorageKey = 'theme'
-
 export const useTheme = (): useThemeReturn => {
-  const storedTheme = localStorage.getItem(localStorageKey)
-  const initialTheme: ThemeType = storedTheme as ThemeType ?? 'light'
+  const storedTheme = localStorage.getItem(LocalStorageKeysConstants.theme)
+  const initialTheme: ThemeType = storedTheme as ThemeType ?? Theme.light
   const [theme, setTheme] = useState<ThemeType>(initialTheme)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const newTheme: ThemeType = e.target.checked ? 'dark' : 'light'
+    const newTheme: ThemeType = e.target.checked ? Theme.dark as ThemeType : Theme.light as ThemeType
     setTheme(newTheme)
-    localStorage.setItem(localStorageKey, newTheme)
+    localStorage.setItem(LocalStorageKeysConstants.theme, newTheme)
   }
 
   useEffect(() => {
