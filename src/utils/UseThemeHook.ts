fba24cf72b87ecbea8cbf46b495db7753e2c0e51
Lilian Marzet <lilian.marzet123@gmail.com>
2023-11-29 00:29:25 +0100

    [FIX] ESLint

--- src/utils/UseThemeHook.ts
+++ src/utils/UseThemeHook.ts
@@ -6,19 +6,19 @@ import { Theme } from '@constants/Constants'
 type useThemeReturn = [ThemeType, () => void]
 
 export const useTheme = (): useThemeReturn => {
-  const storedTheme = localStorage.getItem(LocalStorageKeysConstants.theme)
-  const initialTheme: ThemeType = storedTheme as ThemeType ?? Theme.light
-  const [theme, setTheme] = useState<ThemeType>(initialTheme)
+    const storedTheme = localStorage.getItem(LocalStorageKeysConstants.theme)
+    const initialTheme: ThemeType = storedTheme as ThemeType ?? Theme.light
+    const [theme, setTheme] = useState<ThemeType>(initialTheme)
 
-  const toggleTheme = (): void => {
-    const newTheme: ThemeType = theme === Theme.light ? Theme.dark as ThemeType : Theme.light as ThemeType
-    setTheme(newTheme)
-    localStorage.setItem(LocalStorageKeysConstants.theme, newTheme)
-  }
+    const toggleTheme = (): void => {
+        const newTheme: ThemeType = theme === Theme.light ? Theme.dark as ThemeType : Theme.light as ThemeType
+        setTheme(newTheme)
+        localStorage.setItem(LocalStorageKeysConstants.theme, newTheme)
+    }
 
-  useEffect(() => {
-    document.body.setAttribute('data-theme', theme)
-  }, [theme])
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme)
+    }, [theme])
 
-  return [theme, toggleTheme]
+    return [theme, toggleTheme]
 }
